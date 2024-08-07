a9a158599afc71e24402aaa41d3544d718ec13f1
SahilTalathi142001 <80190527+SahilTalathi142001@users.noreply.github.com>
2024-08-07 16:58:25 +0530

    Update s3_transfer.py

--- s3_transfer.py
+++ s3_transfer.py
@@ -1,17 +1,32 @@
+import os
 from pyspark.sql import SparkSession
 
-# Initialize Spark session
-spark = SparkSession.builder \
-    .appName("parquet_example") \
-    .getOrCreate()
+def main():
+    # Retrieve AWS credentials from environment variables
+    aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+    aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+    aws_session_token = os.getenv('AWS_SESSION_TOKEN')
 
-def write_parquet_file():
-    # Read data from S3
-    df = spark.read.csv('s3://datasource-dataops-group5/vehicles-dataset/vehicles.csv', header=True)
-    
-    # Write data to another S3 bucket
-    df.repartition(1).write.mode('overwrite').parquet('s3://datalake-dataops-group5/')
+    # Initialize Spark session with AWS credentials
+    spark = SparkSession.builder \
+        .appName("S3 to Parquet") \
+        .config("spark.hadoop.fs.s3a.access.key", aws_access_key_id) \
+        .config("spark.hadoop.fs.s3a.secret.key", aws_secret_access_key) \
+        .config("spark.hadoop.fs.s3a.session.token", aws_session_token) \
+        .getOrCreate()
+
+    # S3 bucket information
+    source_s3_bucket = "s3://datasource-dataops-group5/vehicles-dataset/vehicles.csv"
+    destination_s3_bucket = "s3://datalake-dataops-group5/vehicles/"
+
+    # Read CSV data from S3
+    df = spark.read.format("csv").option("header", "true").load(source_s3_bucket)
+
+    # Write data to Parquet format in S3
+    df.write.mode("overwrite").parquet(destination_s3_bucket)
+
+    # Stop Spark session
+    spark.stop()
 
 if __name__ == "__main__":
-    write_parquet_file()
-   
+    main()
