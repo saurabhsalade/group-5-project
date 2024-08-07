990e1bceb71928afe280e3e12a0c6a95de9af8c9
SahilTalathi142001 <80190527+SahilTalathi142001@users.noreply.github.com>
2024-08-07 21:25:01 +0530

    Update ingestion.py

--- ingestion.py
+++ ingestion.py
@@ -1,25 +1,16 @@
-import sys
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
-from awsglue.context import GlueContext
-from awsglue.job import Job
-
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
-job = Job(glueContext)
-job.init(args['JOB_NAME'], args)
-
-# Define the source and target S3 paths
-source_s3_path = "s3://datasource-dataops-group5/vehicles-dataset/"
-target_s3_path = "s3://datalake-dataops-group5/vehicles/"
-
-# Read the data from the source S3 bucket
-datasource0 = glueContext.create_dynamic_frame.from_options("s3", {'paths': [source_s3_path]}, format="csv")
-
-# Write the data to the target S3 bucket
-datasink4 = glueContext.write_dynamic_frame.from_options(frame=datasource0, connection_type="s3", connection_options={"path": target_s3_path}, format="parquet")
-
-job.commit()
+AWSTemplateFormatVersion: '2010-09-09'
+Resources:
+  GlueJob:
+    Type: AWS::Glue::Job
+    Properties:
+      Name: "glue-job-ingestion"
+      Role: "arn:aws:iam::233359079168:role/LabRole"  # Replace with the ARN of the 'Lab Role'
+      Command:
+        Name: "glueetl"
+        ScriptLocation: "s3://datasource-dataops-group5/script-ingestion/ingestion.py"  # Update with your S3 path
+      DefaultArguments:
+        "--job-language": "python"
+      MaxRetries: 0
+      GlueVersion: "2.0"
+      WorkerType: "G.1X"
+      NumberOfWorkers: 5
