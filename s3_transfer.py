8f5b3e46d8f2053b99b6cf276e993a5daaed5ec1
SahilTalathi142001 <80190527+SahilTalathi142001@users.noreply.github.com>
2024-08-07 19:06:17 +0530

    Update s3_transfer.py

--- s3_transfer.py
+++ s3_transfer.py
@@ -3,15 +3,17 @@ from pyspark.sql import SparkSession
 # Initialize Spark session
 spark = SparkSession.builder \
     .appName("S3 to S3 Transfer") \
-    .config("spark.hadoop.fs.s3a.aws.credentials.provider", "com.amazonaws.auth.DefaultAWSCredentialsProviderChain") \
+    .config("spark.hadoop.fs.s3a.access.key", "your-access-key-id") \
+    .config("spark.hadoop.fs.s3a.secret.key", "your-secret-access-key") \
+    .config("spark.hadoop.fs.s3a.session.token", "your-session-token") \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
     .config("spark.hadoop.fs.s3a.multipart.size", "104857600") \
     .config("spark.hadoop.fs.s3a.fast.upload", "true") \
     .getOrCreate()
 
 # Define S3 paths
-source_bucket = "s3://datasource-dataops-group5/vehicles/"
-destination_bucket = "s3://datalake-dataops-group5/vehicles/"
+source_bucket = "s3a://datasource-dataops-group5/vehicles/"
+destination_bucket = "s3a://datalake-dataops-group5/vehicles/"
 
 # Read data from the source S3 bucket
 df = spark.read.option("header", "true").csv(source_bucket)
