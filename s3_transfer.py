318424d7048aecf004f8e92384bbf61f60389d84
SahilTalathi142001 <80190527+SahilTalathi142001@users.noreply.github.com>
2024-08-07 18:27:12 +0530

    Update s3_transfer.py

--- s3_transfer.py
+++ s3_transfer.py
@@ -10,7 +10,7 @@ source_bucket = "s3://datasource-dataops-group5/vehicles/"
 destination_bucket = "s3://datalake-dataops-group5/vehicles/"
 
 # Read data from the source S3 bucket
-df = spark.read.parquet(source_bucket)
+df = spark.read.csv(source_bucket)
 
 # Write data to the destination S3 bucket
 df.write.mode("overwrite").parquet(destination_bucket)
