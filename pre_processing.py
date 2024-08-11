f35d6f75502aa55d43d1dba2665a12d6970c4f78
SahilTalathi142001 <80190527+SahilTalathi142001@users.noreply.github.com>
2024-08-11 18:09:01 +0530

    Update pre_processing.py

--- pre_processing.py
+++ pre_processing.py
@@ -13,12 +13,12 @@ job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
 # Script generated for node Amazon S3
-AmazonS3_node1723198797207 = glueContext.create_dynamic_frame.from_options(format_options={}, connection_type="s3", format="parquet", connection_options={"paths": ["s3://datalake-dataops-group5/vehicles/"]}, transformation_ctx="AmazonS3_node1723198797207")
+AmazonS3_node1723198797207 = glueContext.create_dynamic_frame.from_options(format_options={}, connection_type="s3", format="parquet", connection_options={"paths": ["s3://datalake-dataops-group5-bucket/vehicles/"]}, transformation_ctx="AmazonS3_node1723198797207")
 
 # Script generated for node Drop Fields
 DropFields_node1723199063184 = DropFields.apply(frame=AmazonS3_node1723198797207, paths=["region_url", "image_url", "description"], transformation_ctx="DropFields_node1723199063184")
 
 # Script generated for node Amazon S3
-AmazonS3_node1723199342593 = glueContext.write_dynamic_frame.from_options(frame=DropFields_node1723199063184, connection_type="s3", format="glueparquet", connection_options={"path": "s3://datawarehouse-dataops-group5/vehicles-transform/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_node1723199342593")
+AmazonS3_node1723199342593 = glueContext.write_dynamic_frame.from_options(frame=DropFields_node1723199063184, connection_type="s3", format="glueparquet", connection_options={"path": "s3://datawarehouse-dataops-group5-bucket/vehicles-transform/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_node1723199342593")
 
 job.commit()
