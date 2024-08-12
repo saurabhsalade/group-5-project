79a0a7d5a615865832ba1f7711033e0e43ce1278
SahilTalathi142001 <80190527+SahilTalathi142001@users.noreply.github.com>
2024-08-12 19:59:33 +0530

    Update pre_processing.py

--- pre_processing.py
+++ pre_processing.py
@@ -20,7 +20,7 @@ AmazonS3_node1723463202371 = glueContext.create_dynamic_frame.from_options(
     format_options={},
     connection_type="s3",
     format="parquet",
-    connection_options={"paths": ["s3://datalake-dataops-group5-bucket/vehicles/"], "recurse": True},
+    connection_options={"paths": ["s3://datalake-dbda-group5/vehicles-ingestion/"], "recurse": True},
     transformation_ctx="AmazonS3_node1723463202371"
 )
 
@@ -63,7 +63,7 @@ AmazonS3_node1723463967335 = glueContext.write_dynamic_frame.from_options(
     frame=finalDynamicFrame,
     connection_type="s3",
     format="glueparquet",
-    connection_options={"path": "s3://datawarehouse-dataops-group5-bucket/vehicles-transform/", "partitionKeys": []},
+    connection_options={"path": "s3://datawarehouse-dbda-group5/vehicles-transform/", "partitionKeys": []},
     format_options={"compression": "snappy"},
     transformation_ctx="AmazonS3_node1723463967335"
 )
