cacbe1c4cb7685079dd2d372c001c8a893e60a35
saurabhsalade <70425658+saurabhsalade@users.noreply.github.com>
2024-08-12 15:08:50 +0530

    Update pre_processing.py

--- pre_processing.py
+++ pre_processing.py
@@ -4,7 +4,7 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-
+#
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
